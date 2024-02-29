a7b50ed8a93192bceb14f32d63a9a974f198da69
maab-fw <maabfw@proton.me>
2024-02-29 21:12:31 +0600

    card display fulfilled

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ Single data details: https://openapi.programming-hero.com/api/ai/tool/${id}
 
 Single data Example: https://openapi.programming-hero.com/api/ai/tool/01 */
 
-const loadData = async () => {
+const loadData = async (displayMore) => {
     // try {
     const res = await fetch('https://openapi.programming-hero.com/api/ai/tools')
     const data = await res.json()
     const cards = data.data.tools
-    displayCards(cards)
+    displayCards(cards, displayMore)
     // }
     // catch {
     //     console.log('error occur');
@@ -17,42 +17,52 @@ const loadData = async () => {
 }
 
 
-const displayCards = (cards) => {
+const displayCards = (cards, displayMore) => {
     // console.log(cards);
 
     const sectionContainer = document.getElementById('card-container')
     sectionContainer.textContent = ''
+    const seeMoreDiv = document.getElementById('see-more-div')
 
-    // if (cards.length>6) {
 
-    // }
-    cards = cards.slice(0, 6)
+    if (cards.length > 6 && !displayMore) {
+        cards = cards.slice(0, 6)
+        seeMoreDiv.classList.remove('hidden')
+    }
 
+    else {
+        seeMoreDiv.classList.add('hidden')
+    }
     cards.forEach(card => {
         console.log(card);
-
+        card.features.map(feature => console.log(`<li>${feature}</li>`))
         const oneCard = document.createElement('div')
         oneCard.classList = `card w-full bg-base-100 p-6 shadow-xl`
         oneCard.innerHTML = `
-                <figure class="h-full"><img class="w-full" src="${card.image}" alt="image" />
-                </figure>
-                <div class="mt-6">
-                    <h2 class="card-title">Features</h2>
-                    <ol class="list-decimal">
-                        <li>${card.features[0] || ""}</li>
-                        <li>${card.features[1] || ""}</li>
-                        <li>${card.features[2] || ""}</li>
-                        <li>${card.features[3] || ""}</li>
-                        <li>${card.features[4] || ""}</li>
-                    </ol>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+        <figure class="h-full"><img class="w-full" src="${card.image || ""}" alt="image" />
+        </figure>
+        <div class="mt-6">
+            <h2 class="card-title">Features</h2>
+            <ol id="LL" class="list-decimal pl-4 mt-4">
+                ${card.features.map(feature => `<li>${feature}</li>`).join('')}
+            </ol>
+            <hr class="my-6">
+            <div class="flex items-center justify-between">
+                <div>
+                    <h2 class="mb-4 text-2xl font-semibold">${card.name}</h2>
+                    <p>${card.published_in}</p>
                 </div>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-lg btn-circle ">
+                    <i class="fa-solid fa-arrow-right text-[rgb(235,87,87)]"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
         `
 
         // const cardFeaturesArray = card.features
-        // const ol = document.getElementsByTagName('ol')
+        // const ol = document.getElementById('LL')
         // console.log(ol);
         // const cardFeatures = cardFeaturesArray.forEach(arrEle => {
         //     const li = document.createElement('li')
@@ -64,5 +74,10 @@ const displayCards = (cards) => {
     })
 }
 
+
+const seeMore = () => {
+    displayMore = true
+    loadData(displayMore)
+}
 
 loadData()
